c5cc9bda4d0eb461732c4482f456fea5366b6fe4
yuvaraj3211 <rekapalliyuvaraj8@gmail.com>
2026-07-14 21:32:07 +0530

    Add files via upload

---
+++ user inputs.py
@@ -0,0 +1,51 @@
+'''a=2
+b=4
+print(a+b)'''
+
+'''a=3
+b=8
+c=a+b
+print(c)'''
+
+#run_time input()
+
+'''a=int(input("enter a value"))
+b=int(input("enter b value"))
+print(a+b)'''
+
+'''a=float(input("enter a value"))
+b=float(input("enter b value"))
+print(a+b)'''
+
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''fname=input("first name")
+lname=input("last name")
+print(fname+lname)'''
+
+'''city=input("city name")
+print(city)'''
+
+'''a=complex(input("enter a value"))
+b=complex(input("enter b value"))
+print(a+b)'''
+
+'''a=bool(input("enter a value"))
+b=bool(input("enter b value"))
+c=bool(input("enter c value"))
+print(a+b+c)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a=input()
+print(a)'''
+
+
