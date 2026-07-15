51427c44f8e8c2b949f5d66a1e80c3b9ef8b4a45
yuvaraj3211 <rekapalliyuvaraj8@gmail.com>
2026-07-15 20:59:49 +0530

    Add files via upload

---
+++ input users 1.py
@@ -0,0 +1,86 @@
+'''a=int(input("a value: "))
+b=int(input("b value: "))
+option=input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+#swapping of two numbers
+'''a=int(input())
+b=int(input())
+c=a
+a=b
+b=c
+print(a)
+print(b)'''
+
+'''a=int(input())
+b=int(input())
+a,b=b,a
+print(a,b)'''
+
+'''a=int(input())
+b=int(input())
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)'''
+
+'''a=int(input())
+b=int(input())
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+print("after swapping a=%d,b=%d" %(a,b))'''
+
+'''a=["codegnan","python","course"]
+b=str(a)
+print(b.upper())'''
+
+'''a=[10,20,30,40,50,"code"]
+[10,20,30,40,50,"c","o","d","e"]'''
+
+'''a=[10,20,30,40,50,"code"]
+a.extend("code")
+print(a)'''
+
+'''a=[5,7,9,10,11,13]
+[5,7,9,10,11,12,13]'''
+
+'''a=[5,7,9,10,11,13]
+a.insert(5,12)
+print(a)'''
+
+'''a=[9,7,4,0,1,5,10,8,6,3]
+#[0,1,2,3,4,5,6,7,8,9,10]
+
+a.sort()
+print(a)'''
+
+'''a=("apple","banana","mango")
+#("apple","banana","mango","grapes")
+
+b=list(a)
+b.append("grapes")
+c=tuple(b)
+print(c)'''
+
+#student profile
+
+idno=int(input("enter idno: "))
+name=input("enter name: ")
+mobile=int(input("enter mobile: "))
+mailid=input("enter mail: ")
+college=input("enter college: ")
+branch=input("branch: ")
+print("idno: ",idno)
+print("name: ",name)
+print("mobile: ",mobile)
+print("mailid: ",mailid)
+print("college: ",college)
+print("branch: ",branch)
+
+
