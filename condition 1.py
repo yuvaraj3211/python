03ca48957817c7e111f81b6099a7fd8a2c22a8b4
yuvaraj3211 <rekapalliyuvaraj8@gmail.com>
2026-07-17 20:36:16 +0530

    Add files via upload

---
+++ condition 1.py
@@ -0,0 +1,204 @@
+#if-elif conditions by using comparision
+
+'''a=2
+b=4
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a=2
+b=4
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+'''a=2
+b=4
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+#if-elif-else
+
+'''a=5
+b=10
+if a>b:
+    print("greater")
+elif a!=b:
+    print("not equal")
+else:
+    print("true")'''
+
+'''a=5
+b=10
+if a>b:
+    print("greater")
+elif a>=b:
+    print("not equal")
+elif a==b:
+    print("equal")
+else:
+    print("true")'''
+
+'''a=5
+b=10
+if a<b and b>a:
+    print("greater")
+elif a!=b or a==b:
+    print("not equal")
+else:
+    print("true")'''
+
+'''a=int(input("a value"))
+if type(a) is int:
+    print("it is int")
+else:
+    print("not int")'''
+
+#multiple if
+
+'''a=9
+b=11
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a==b:
+    print("equal")'''
+
+'''a=9
+b=11
+if a>b:
+    print("less")
+if b>a:
+    print("greater")
+if a==b:
+    print("equal")
+if a!=b:
+    print("not equal")'''
+
+#nested-if
+
+'''a=2
+b=4
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=2
+b=4
+if a==b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a=12
+b=14
+if a<b:
+    print("less")
+    if b<a:
+        print("greater")'''
+
+'''a=15
+b=20
+if a<b:
+    print("less")
+    if b<a:
+        print("greater")
+    else:
+        print("true")'''
+
+'''a=2
+b=4
+if a==b:
+    print("less")
+    if b>a:
+        print("greater")
+else:
+    print("true")'''
+
+'''a=2
+b=4
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+    elif a!=b:
+        print("not equal")'''
+
+#tasks
+
+#voting
+
+'''a=int(input("give age: ")
+if a>=18:
+    print("eligible for vote")
+else:
+    ("not eligible for vote")'''
+
+#even and odd
+
+'''a=int(input("enter a number: ")
+if a%2==0:
+      print("even")
+else:
+    print("odd")'''
+
+#leap year
+
+'''a=int(input("enter year: "))
+if a%4==0:
+      print("leap year")
+else:
+    print("not leap year")'''
+
+#guest code
+
+'''a=input("enter name")
+if a=="yuvaraj":
+    print("welcome yuvaraj")
+else:
+    print("welcome guest")'''
+
+'''a=['yuva','raj','rekaplli']
+b=input("enter name: ")
+if b in a:
+    print("welcome",b)                         
+else:
+    print("welcome guest")'''
+
+#vowel or consonent
+
+a=['a','e','i','o','u']
+n=input("enter a letter")
+if n in a:
+    print("vowel")
+else:
+    print("consonent")
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
