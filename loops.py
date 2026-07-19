d43d04f1265b10aa67a788f911d975fce3b95bb1
yuvaraj3211 <rekapalliyuvaraj8@gmail.com>
2026-07-19 11:59:57 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,88 @@
+#loops
+#for,while,range,break,continue,pass
+
+#for loop
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a=(10,20,30,40,50)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={10,20,30,40,50}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''a={"name":"pooja","city":"vja","state":"ap","year":2026}
+for i in a:
+    print(i)
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=[5.6,7.8]
+for i in a():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
