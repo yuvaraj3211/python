ba52316db4b4e20396b67923216fb859048d6345
yuvaraj3211 <rekapalliyuvaraj8@gmail.com>
2026-07-19 11:59:13 +0530

    Add files via upload

---
+++ condition 4.py
@@ -0,0 +1,73 @@
+#social media login
+
+'''a=input()
+b=int(input())
+if a=="yuvaraj":
+    if b==123:
+        print("login successfull")
+else:
+    print("invalid crediants")'''
+
+'''a=input()
+b=int(input())
+if a=="yuvaraj":
+    if b==123:
+        print("login successfull")
+    else:
+        print("incorrect password")
+else:
+    print("invalid crediats")'''
+
+'''a=input()
+b=int(input())
+if a=="yuvaraj" and b==123:
+    print("login successfull")
+else:
+    print("invalid crediants")'''
+
+'''a=int(input())
+b=int(input())
+c=int(input())
+if a>=18:
+    print("eligible for vote")
+if b>=70:
+    print("allow to write exams")
+if c>=80:
+    print("they are eligible for scholarship")'''
+
+'''a=int(input())
+b=int(input())
+c=int(input())
+if a>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
+if b>=70:
+    print("allow to write exams")
+if c>=80:
+    print("they are eligible for scholarship")'''
+
+'''a=input("cake name: ")
+if a=="red velvet":
+    print("price is 1200")
+elif a=="choco almond":
+    print("price is 1000")
+elif a=="honey almond":
+    print("price is 800")
+elif a=="butter scotch":
+    print("price is 600")
+else:
+    print("cake is not available")'''
+
+'''a=int(input("enter a price: "))
+if a==1000:
+      print("bbq pizza")
+elif a==800:
+      print("crispy chicken pizza")
+elif a==600:
+      print("paneer pizza")
+elif a==400:
+      print("corn pizza")
+elif a==200:
+      print("french fries&coke")'''
+
