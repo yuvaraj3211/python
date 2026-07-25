7b0ba330bca4716a2beba908cbc29bc8525010a2
yuvaraj3211 <rekapalliyuvaraj8@gmail.com>
2026-07-25 10:26:10 +0530

    Add files via upload

---
+++ loops 1.py
@@ -0,0 +1,152 @@
+'''a=["python","java","dsa"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+#while loop()
+
+'''a=10
+while a<1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a=20
+while a>2:
+    a=a-1
+print(a)'''
+
+'''a=20
+while a>2:
+    print(a)
+    a=a-1'''
+
+'''a=20
+while a>2:
+    a=a-1
+    print(a)'''
+
+'''a=30
+while a>5:
+    print(a)
+    a=a+1'''
+
+'''a=30
+while a>5:
+    print(a)
+     a=a-1'''
+
+'''a=1
+while a<30:
+    print(a)
+     a=a+1'''
+
+#voting
+
+'''while True:
+    age=int(input("enter age"))
+    if age>=18:
+        print("eligible for vote")
+    else:
+        print("not eligible for vote")'''
+
+#range():the range function returns a sequence of numbers starting from 0 by default and increments by 1 by 1 and stops before a specified number
+
+#start-stop-step
+
+'''for i in range(11):
+    print(i)'''
+
+'''for i in range(5,20):
+    print(i)'''
+
+'''for i in range(0,20,2):
+    print(i)'''
+
+'''for i in range(5,50,5):
+    print(i)'''
+
+'''for i in range(3,30,3):
+    print(i,end=" ")'''
+
+#STUDDENT GRADES
+
+'''a=int(input("enter the marks"))
+b=[];c=[];d=[];e=[];f=[]
+for i in range(52):
+    b.append(i)
+for i in range(91,102,1):
+    c.append(i)
+for i in range(81,92,1):
+    d.append(i)
+for i in range(71,82,1):
+    e.append(i)
+for i in range(50,72,1):
+    f.append(i)
+if a in b:
+    print("Fail")
+elif a in c:
+    print("A")
+elif a in d:
+    print("B")
+elif a in d:
+    print("C")
+else:
+    print("D")'''
+
+'''while True:
+    marks=int(input("enter marks"))
+    if marks in range(91,101):
+        print("grade a")
+    elif marks in range(81,91):
+        print("grade b")
+    elif marks in range(71,81):
+        print("grade c")
+    elif marks in range(50,71):
+        print("grade d")
+    else:
+        print("fail,study well.....")'''
+
+for i in range(10):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+    
+    
+    
+    
+
